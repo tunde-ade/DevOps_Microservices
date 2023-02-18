94df6609c032671d8ca2215a51276f908b25f420
EC2 Default User <ec2-user@ip-172-31-23-58.ec2.internal>
2023-02-18 14:29:25 +0000

    project 4 upload

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=obat_proj4 .
+docker build --tag=obatproj .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 obat_proj4
+docker run -p 8000:80 obatproj
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="tundeade/obatproj:v1"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag obatproj $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
